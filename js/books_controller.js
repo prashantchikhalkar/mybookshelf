fe68b71dbf0cfd170d36dff213258eb7654d791b
prashant <prashantchikhalkar@gmail.com>
2018-08-27 12:11:11 +0530

    added 2 new books
    Added,
    1) Rich Dad Poor Dad
    2) The Daily Stoic

--- js/books_controller.js
+++ js/books_controller.js
@@ -20,6 +20,24 @@ app.controller('BooksController', ['$scope', function($scope) {
 			'mediumPostUrl': '',
 			'read': true
 		}
+		
+		{
+			'title': 'Rich Dad Poor Dad: What the Rich Teach their Kids About Money that the Poor and Middle Class Do Not!',
+			'author': 'Robert T Kiyosaki',
+			'url': 'https://www.amazon.in/Rich-Dad-Poor-Middle-Updates/dp/1612680194',
+			'hasMediumPost': false,
+			'mediumPostUrl': '',
+			'read': true
+		}
+		
+		{
+			'title': 'The Daily Stoic: 366 Meditations on Wisdom, Perseverance, and the Art of Living: Featuring new translations of Seneca, Epictetus, and Marcus Aurelius',
+			'author': 'Ryan Holiday',
+			'url': 'https://www.amazon.in/Daily-Stoic-Meditations-Perseverance-translations/dp/1781257655',
+			'hasMediumPost': false,
+			'mediumPostUrl': '',
+			'read': true
+		}
 	];
 
 }]);
